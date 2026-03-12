8b13f3587f20161c2c533134b548f906d313a80c
ColinTugend-ASU <ctugend@asu.edu>
2026-03-12 16:14:38 -0400

    Update .json files to match the changes in the most recent version of dev
    # Conflicts:
    #	IBDC-expo/package-lock.json
    #	IBDC-expo/package.json

--- IBDC-expo/hooks/BikeSensorProtoDriver.ts
+++ IBDC-expo/hooks/BikeSensorProtoDriver.ts
@@ -1,5 +1,6 @@
 import * as net from 'net';
 import * as protobuf from 'protobufjs';
+import * as readline from 'readline-sync'
 
 // Load arguments from command line
 /*const args = process.argv.slice(2);
@@ -24,7 +25,7 @@ else console.log('Using default values of host:localhost port:8000');
 */
 
 //proto_testing_loop();
-initialize();
+main();
 
 
 function proto_testing_loop (socket?: net.Socket)
@@ -103,7 +104,7 @@ function initialize(port: number = 8000, hostname: string = 'localhost', attempt
 // Send protobuf message to user phone and wait x time for a response
 // RETURNS true if message was sent over the wire. NO guarantees the message reaches the destination, only that it was sent
 function send_status_update(sock:net.Socket, wait?: number, ): boolean {
-    protobuf.load("IBDC.proto", function (err, root) {
+    protobuf.loadSync("../Resources/IBDC.proto", function (err, root) {
         if (err)
             throw err;
 
@@ -144,19 +145,62 @@ function send_status_update(sock:net.Socket, wait?: number, ): boolean {
 // The main function encapsulates all the logic required from when the IBDC sensor connects to the mobile application
 // via bluetooth.
 function main() {
-    console.log("Main function entered");
-    const socket = initialize();
+    const socket: net.Socket = initialize();
     if (!socket) {
         console.log("Initialization failed. Exiting.")
         process.exit(0);
     }
     // The program will now listen for messages from the mobile application and respond to them with data.
-    let finished = false;
-    while (!finished) {
-        const message = socket.read();
+    // Communications loops
+    // 1. Add buffer from console to messages
+    // 2. Read buffer from socket to messages
+    // 3. While the message buffer is > 0
+    // 4. Parse message and send data back to the phone over the socket
+    // 5. Repeat until bluetooth connection is dropped
+    let connected:booleean = true;
+    let messages: string[] = [];
+    while (connected) {
+        console.log('Embedded Message Types:\n' +
+                    'EventNotification\n' +
+                    'ImageChunk\n' +
+                    'DeviceStatus\n');
+        const embeddedEventNotification =  readline.question('Enter an embedded message type or enter nothing to end the connection. ');
+        protobuf.roots;
+
     }
-
-
 
 }
 
+
+/*
+## The ProtoDriver Module  (Work in Progress)
+The ProtoDriver Module allows message passing via Protobuf over a network. This module can be used as a way to model the
+way the IBDC Bike Sensor communicates with the mobile application. This driver will be able to receive signals
+meant for the IBDC Bike Sensor from both the embedded system(modeled as console inputs) and messages from the mobile application.
+This module can be used for testing the serialization of data between the devices and the logic required in both devices
+based on this communication.
+
+## How to use the ProtoDriver Module (Phase 1)
+    - Once the BikeSensorProtoDriver.ts is started, the script will automatically conncect to localhost::8000. The driver
+    will send a DeviceStatus message to the device. To verify the program connects to a host listening on a port on localhost,
+    use this command in the terminal to send a listener to localhost::8000 (I'm on Mac so I'm not sure if this will work for everyone)
+     -> nc -l -p 8000
+
+
+## To Dos
+    - Program the ability to allow console input in order to mimic embedded messages from the IBDC sensor
+    - Deserialize messages from over the wire
+    - Serialize and send messages based on a message from the mobile application or console input
+    - Architect a better control flow for the program. It's kind of rough right now, but it's going in the right direction.
+
+
+## Information Sources
+
+     https://protobuf.dev/programming-guides/proto3/
+        -Explains the rules and syntax for our version of Protobuf.
+
+    https://www.npmjs.com/package/protobufjs#valid-message
+        Explains how to download the Protobuf package and it's dependencies.
+        Also contains a guide on how to use the library in different ways.
+        This is where I copy and pasted the code I cited in the BikeSensorProtoDriver.ts file.
+ */
